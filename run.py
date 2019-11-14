bb2f1eec9353561d70463f18dc0f18d50522a7a3
Matt <5638426+mattbruv@users.noreply.github.com>
2019-11-14 15:30:42 -0500

    problems 41, 43, 46, 98

--- run.py
+++ run.py
@@ -1,26 +1,29 @@
-import time
+from datetime import datetime
 import sys
 
 code = """
 from src import problem{}
-problem{}.problem{}()
+problem{}.problem{}(args)
 """
 
 # Runs a problem number and returns execution time
-def run(p):
-    start = time.time() 
+def run(p, args):
+    start = datetime.now()
     print('START PROBLEM', p)
     try:
-        exec(code.replace('{}', p))
+        if len(args) == 0:
+            args = ''
+        exec(code.replace('{}', p).replace('args', str(args)))
     except KeyboardInterrupt:
-        print('!!! CANCELLED EXECUTION AFTER ', p, 'SECONDS !!!')
-    return round(time.time() - start, 5)
+        print('!!! CANCELLED EXECUTION AFTER', datetime.now() - start)
+    return datetime.now() - start
 
 if __name__ == "__main__":
     try:
         problemNumber = sys.argv[1]
+        problemArgs = sys.argv[2:]
     except:
         print('Invalid problem number given!')
 
-    timer = run(problemNumber)
-    print(timer, 'SECONDS ELAPSED')
+    timer = run(problemNumber, problemArgs)
+    print(timer, 'ELAPSED')
