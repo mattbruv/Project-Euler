0efcf8e64f36c17fd6084cfdad00f8ab15b10c40
Matthew <5638426+mattbruv@users.noreply.github.com>
2019-12-26 09:03:10 -0500

    monopoly progress

--- src/problem84.py
+++ src/problem84.py
@@ -18,15 +18,32 @@ this needs to be cycled as if it were played ingame.
 # and GO (3.09%) = Square 00.
 # So these three most popular are the six-digit modal string: 102400.
 
-def turn(currentSquare):
-    nextSquare = randrange(40)
-    return nextSquare
+diceSides = 6
+
+GO = 0
+JAIL = 10
+C1 = 11
+E3 = 24
+H2 = 39
+R1 = 5
+
+def processSquare(nextSquare):
+    result = nextSquare
+    if nextSquare == 30:
+        result = 10
+    return result
 
 def communityChest(squareAt):
+    squares = [0, 10] 
     return 0
 
 def chance(squareAt):
+    BACK = (squareAt - 3) % 40
+    squares = [GO, JAIL, C1, E3, H2, R1, BACK]
     return 0
+
+def roll():
+    return (randrange(1, diceSides + 1), randrange(1, diceSides + 1))
 
 def problem84():
     turns = int(1e5)
@@ -36,8 +53,19 @@ def problem84():
         visits[i] = 0
 
     currentSquare = 0 # start on GO
+    consecutiveDoubles = 0
+
     for i in range(0, turns):
-        currentSquare = turn(currentSquare)
+        d1, d2 = roll()
+        if d1 == d2: # we rolled doubles
+            consecutiveDoubles += 1
+            if consecutiveDoubles == 3:
+                consecutiveDoubles = 0
+                currentSquare = 10
+        else:
+            nextSquare = ((d1 + d2 + currentSquare) % 40)
+            currentSquare = processSquare(nextSquare)
         visits[currentSquare] += 1
+
     print('monopoly odds')
     print(visits)
