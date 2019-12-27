cd01aec7a0d770cf40148727426c284446697fe7
Matthew <5638426+mattbruv@users.noreply.github.com>
2019-12-27 11:35:52 -0500

    problem 47

--- src/helpers/prime.py
+++ src/helpers/prime.py
@@ -8,3 +8,16 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
+
+def primeFactors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
