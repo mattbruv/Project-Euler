2574391a0bac38d270a3262a8d722d337feb348e
Matt <5638426+mattbruv@users.noreply.github.com>
2020-01-23 12:17:34 -0500

    problem 73

---
+++ src/problem73.py
@@ -0,0 +1,34 @@
+from fractions import Fraction
+from src.helpers.division import GCD
+
+def problem73():
+    total = 0
+    lowerBound = Fraction(numerator=1, denominator=3)
+    upperBound = Fraction(numerator=1, denominator=2)
+    for d in range(1, 12001):
+    #for d in range(1, 9):
+        if d % 100 == 0:
+            print(d)
+        half = d // 2
+        if half <= 1:
+            continue
+        # print(f > lowerBound) possibly >=
+        # print(f < upperBound)
+        n = half
+        while True:
+            f = Fraction(numerator=n, denominator=d)
+            if f == upperBound: #skip if half
+                #print(f, 'equals', upperBound)
+                n -= 1
+                continue
+            if f <= lowerBound:
+                #print(f, 'lower than', lowerBound)
+                break
+            if GCD(n, d) == 1:
+                #print(f)
+                total += 1
+            n -= 1
+    print(total)
+
+# 7295372
+# 0:06:42.664031 ELAPSED
