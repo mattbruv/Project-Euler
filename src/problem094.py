13f48e93bdf5ee9f9dceca4a4503487717df2d76
Matt <5638426+mattbruv@users.noreply.github.com>
2020-09-17 08:05:35 -0400

    94

--- src/problem094.py
+++ src/problem094.py
@@ -1,20 +1,23 @@
+from decimal import *
+from math import sqrt
 
 def area(a, b, c):
-    s = (a + b + c) / 2
-    return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    s = Decimal((a + b + c)) / Decimal(2)
+    return Decimal(s * (s - a) * (s - b) * (s - c)).sqrt()
 
 def problem094():
     limit = int(1e9 / 3) + 2
     print(limit)
-    #limit = 10
     total = 0
     for i in range(2, limit):
         for offset in [-1, 1]:
             a = area(i, i, i + offset)
-            if a.is_integer():
-                # it is equilateral
+            if a == int(a):
+                print(a, i)
                 perimeter = i + i + (i + offset)
                 if perimeter < int(1e9):
-                    print(i, a, offset)
                     total += perimeter
     print(total)
+
+# 518408346
+# 2:05:54.694104 ELAPSED
